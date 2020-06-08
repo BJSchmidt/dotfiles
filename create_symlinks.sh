377799799690a47f412e8a978202b1c298021839
Ben Schmidt <benschmidt@benschmidt.tech>
2020-06-08 14:17:37 -0600

    Added script to create symlinks on a new machine

---
+++ create_symlinks.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Dotfile Symlinks
+ln -s ~/repos/dotfiles/.bashrc ~/.bashrc -f
+ln -s ~/repos/dotfiles/.doom.d ~/.doom.d -f
+ln -s /mnt/c/Users/BenSchmidt/Nextcloud/Documents/zettels/ ~/zettels
