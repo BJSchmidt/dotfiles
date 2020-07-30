7dbc535a4dc1b10bdc0038eedce54accd3433d80
Ben Schmidt <benschmidt@benschmidt.tech>
2020-07-30 15:55:52 -0600

    Added TODO for WSL

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -2,4 +2,7 @@
 # Dotfile Symlinks
 ln -s ~/repos/dotfiles/.bashrc ~/.bashrc -f
 ln -s ~/repos/dotfiles/.doom.d ~/.doom.d -f
+
+# TODO Needs to be updated to check for running under WSL, and if so grab the correct Windows User Profile Path for nextcloud / zettels link.
+# See .bashrc for example of WSL1 & 2 checks
 ln -s /mnt/c/Users/BenSchmidt/Nextcloud/Documents/zettels/ ~/zettels
