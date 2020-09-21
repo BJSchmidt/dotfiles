a87a73f186212d356e79b519635cb839698f0d54
Ben Schmidt <benschmidt@benschmidt.tech>
2020-09-21 09:38:43 -0600

    Updates from Go2

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 # Dotfile Symlinks
 ln -s ~/repos/dotfiles/.bashrc ~/.bashrc -f
-ln -s ~/repos/dotfiles/.doom.d ~/.doom.d -f
+
+rm -r ~/.doom.d/
+ln -s ~/repos/dotfiles/.doom.d/ ~/.doom.d
 
 # TODO Needs to be updated to check for running under WSL, and if so grab the correct Windows User Profile Path for nextcloud / zettels link.
 # See .bashrc for example of WSL1 & 2 checks
